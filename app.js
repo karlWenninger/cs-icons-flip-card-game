5c36e18067f893a6b8b4cc6644b807976570f9ca
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-15 09:27:29 -0400

    Fixed consecutive click bug, changed all icons to coder-relevant, added icon colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- const icons = ["fas fa-crown", "fas fa-drum", "fas fa-child", "fas fa-music", "fas fa-bolt", "fas fa-socks", "fas fa-car-crash", "fas fa-glasses"]
+ const icons = ["fas fa-tree", "fas fa-bug", "fas fa-robot", "fas fa-link", "fas fa-laptop", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"]
 
  const mainContainer = document.querySelector('#main-contain')
 
@@ -9,9 +9,9 @@
          iconIndexArr.push(i);
      }
      // dupe each index
-     iconIndexArr = iconIndexArr.concat(iconIndexArr)
+     iconIndexDupeArr = iconIndexArr.concat(iconIndexArr)
      // random sort indexes
-     iconIndexArr.sort((a, b) => 0.5 - Math.random())
+     iconIndexDupeArr.sort((a, b) => 0.5 - Math.random())
 
      // make HTML elements
      const table = document.createElement('table');
@@ -21,12 +21,12 @@
      cellContent.style.visibility = 'hidden';
 
      // make rows, populate cells with icons
-     let rowWidth = Math.sqrt(iconIndexArr.length);
+     let rowWidth = Math.sqrt(iconIndexDupeArr.length);
      let index = 0;
-     while (index < iconIndexArr.length - 1) {
+     while (index < iconIndexDupeArr.length - 1) {
          row.innerHTML = '';
          for (let i = 0; i < rowWidth; i++) {
-             cellContent.classList = `${icons[iconIndexArr[index]]} ${index}`
+             cellContent.classList = `${icons[iconIndexDupeArr[index]]} ${index}`
              cell.appendChild(cellContent)
              cell.classList = 'cell-content';
              row.appendChild(cell.cloneNode(true));
@@ -41,19 +41,20 @@
      makeGrid()
  });
 
+ // track user clicks
  let firstClick;
  mainContainer.addEventListener('click', (e) => {
-     if (e.target.classList.contains('cell-content')) {
+     if (e.target.className = 'cell-content') {
          clickedContent = e.target.firstElementChild;
          clickedContent.style.visibility = 'visible';
-         console.log(clickedContent.classList[2]);
-
-
+         // fontawesome classes match
          if (firstClick != null && firstClick.classList[1] == clickedContent.classList[1]) {
-             firstClick.style.visibility = 'visible';
-             clickedContent.style.visibility = 'visible';
-             return firstClick = null;
-
+             // consecutive clicks on same cell do not return match
+             if (firstClick.classList[2] != clickedContent.classList[2]) {
+                 firstClick.style.visibility = 'visible';
+                 clickedContent.style.visibility = 'visible';
+                 return firstClick = null;
+             }
          } else {
              firstClick = clickedContent;
          };
