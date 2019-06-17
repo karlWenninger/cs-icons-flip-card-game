d5eccc8972248561687c0f86483ddcd724afb4bf
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-16 23:34:24 -0400

    Changed table layout to CSS Grid, needs minor size/spacing adjustments. Bug: click on cell before setTimeout() ends

--- app.js
+++ app.js
@@ -14,52 +14,43 @@
      iconIndexDupeArr.sort((a, b) => 0.5 - Math.random())
 
      // make HTML elements
-     const table = document.createElement('table');
-     const row = document.createElement('tr');
-     const cell = document.createElement('td');
+     const gridContainer = document.createElement('div');
+     gridContainer.className = 'grid-container';
+
+     const cell = document.createElement('div');
+     cell.className = 'grid-item';
+
      const cellContent = document.createElement('i');
      cellContent.style.visibility = 'hidden';
-
-     // make rows, populate cells with icons
-     let rowWidth = Math.sqrt(iconIndexDupeArr.length);
-     let index = 0;
-     while (index < iconIndexDupeArr.length - 1) {
-         row.innerHTML = '';
-         for (let i = 0; i < rowWidth; i++) {
-             cellContent.classList = `${icons[iconIndexDupeArr[index]]} ${index}`
-             cell.appendChild(cellContent)
-             cell.classList = 'cell-content';
-             row.appendChild(cell.cloneNode(true));
-             index++;
-         }
-         table.appendChild(row.cloneNode(true));
+     for (let i = 0; i < iconIndexDupeArr.length; i++) {
+         cellContent.classList = icons[iconIndexDupeArr[i]] + ' ' + i;
+         cell.appendChild(cellContent);
+         gridContainer.appendChild(cell.cloneNode(true));
      }
-     mainContainer.appendChild(table)
+
+     mainContainer.appendChild(gridContainer);
  };
 
  document.addEventListener('DOMContentLoaded', () => {
      makeGrid()
  });
 
- // track user clicks
- let firstClick;
+ // track user clicks. click order: if no match lastClick becomes firstClick
+ let lastClick, firstClick;
  mainContainer.addEventListener('click', (e) => {
-     if (e.target.className = 'cell-content') {
-         clickedContent = e.target.firstElementChild;
-         clickedContent.style.visibility = 'visible';
-         // fontawesome classes match
-         if (firstClick != null && firstClick.classList[1] == clickedContent.classList[1]) {
+     if (e.target.className = 'grid-item') {
+         lastClick = e.target.firstChild;
+         lastClick.style.visibility = 'visible';
+         if (firstClick != null && firstClick.classList[1] == lastClick.classList[1]) {
              // consecutive clicks on same cell do not return match
-             if (firstClick.classList[2] != clickedContent.classList[2]) {
+             if (firstClick.classList[2] != lastClick.classList[2]) {
                  firstClick.style.visibility = 'visible';
-                 clickedContent.style.visibility = 'visible';
                  return firstClick = null;
              }
-         } else {
-             firstClick = clickedContent;
-         };
+         } else { firstClick = lastClick; }
+
          setTimeout(() => {
-             clickedContent.style.visibility = 'hidden';
+             return lastClick.style.visibility = 'hidden';
          }, 500)
-     };
+     }
  });
