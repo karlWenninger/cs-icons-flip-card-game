21edc243a1028fdfc491b17d17b64d6abde3e8b9
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-18 20:47:37 -0400

    Changed CS Grid to flexbox layout, simpler for a simple table-like grid.

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
  const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"]
 
- const mainContainer = document.querySelector('#main-contain')
+ const mainContain = document.querySelector('#main-contain')
+
+ const flexContain = document.createElement('div');
+ flexContain.className = 'flex-contain';
 
  function makeGrid() {
      // make icon index array
      let iconIndexArr = [];
      for (let i = 0; i < icons.length; i++) {
          iconIndexArr.push(i);
-     }
+     };
      // dupe each index
      iconIndexDupeArr = iconIndexArr.concat(iconIndexArr)
      // random sort indexes
      iconIndexDupeArr.sort((a, b) => 0.5 - Math.random())
 
      // make HTML elements
-     const gridContainer = document.createElement('div');
-     gridContainer.className = 'grid-container';
+     const row = document.createElement('div');
+     row.className = 'row';
 
      const cell = document.createElement('div');
-     cell.className = 'grid-item';
+     cell.className = 'cell';
 
      const cellContent = document.createElement('i');
      cellContent.style.visibility = 'hidden';
-     for (let i = 0; i < iconIndexDupeArr.length; i++) {
-         cellContent.classList = icons[iconIndexDupeArr[i]] + ' ' + i;
-         cell.appendChild(cellContent);
-         gridContainer.appendChild(cell.cloneNode(true));
-     }
 
-     mainContainer.appendChild(gridContainer);
+     // poplulate cells w icons, rows w cells, flexContain w rows
+     let cellCount = 1;
+     for (let i of iconIndexDupeArr) {
+         cellContent.classList = `${icons[i]} ${cellCount}`;
+         cell.appendChild(cellContent);
+         row.appendChild(cell.cloneNode(true))
+         if (cellCount % 4 == 0) {
+             flexContain.appendChild(row.cloneNode(true));
+             row.innerHTML = null;
+         }
+         cellCount++;
+     };
+     mainContain.appendChild(flexContain);
  };
 
  document.addEventListener('DOMContentLoaded', () => {
@@ -37,8 +47,8 @@
 
  // track user clicks. click order: if no match lastClick becomes firstClick
  let lastClick, firstClick;
- mainContainer.addEventListener('click', (e) => {
-     if (e.target.className = 'grid-item') {
+ flexContain.addEventListener('click', (e) => {
+     if (e.target.className = 'cell') {
          lastClick = e.target.firstChild;
          lastClick.style.visibility = 'visible';
          if (firstClick != null && firstClick.classList[1] == lastClick.classList[1]) {
