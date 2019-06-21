a126cada74cedef5fc4dff4b1fa23179c31537e7
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-21 15:27:34 -0400

    Refactored/clean up. 99 lines of code on the wall!

--- app.js
+++ app.js
@@ -1,89 +1,83 @@
- const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-microchip", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"]
+ const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"]
 
  const mainContain = document.querySelector('#main-contain')
 
- let flexContain = document.createElement('div');
- flexContain.className = 'flex-contain';
+ const clickCountDisplay = document.querySelector('#click-count-display');
+
+ const gameClockDisplay = document.querySelector('#game-clock-display')
+
+ let flexTable = document.createElement('div');
+ flexTable.className = 'flex-table';
 
  function makeGrid() {
+     // initial stat display
+     totalClicks = 0;
+     userMatches = 0;
+     clickCountDisplay.innerText = `${totalClicks} clicks`;
+     gameClockDisplay.innerText = 'time 0:00'
+
      // make icon index array
      let iconIndexArr = [];
-     for (let i = 0; i < icons.length; i++) {
+     for (let i in icons) {
          iconIndexArr.push(i);
      };
-     // dupe each index
-     iconIndexDupeArr = iconIndexArr.concat(iconIndexArr)
+     // dupe entire array (each icon loads in grid twice)
+     iconIndexDupeArr = iconIndexArr.concat(iconIndexArr);
      // random sort indexes
-     iconIndexDupeArr.sort((a, b) => 0.5 - Math.random())
-
-     // make HTML elements
+     iconIndexDupeArr.sort((a, b) => 0.5 - Math.random());
+     // create HTML elements
      const row = document.createElement('div');
      row.className = 'row';
-
      const cell = document.createElement('div');
      cell.className = 'cell';
-
      const cellContent = document.createElement('i');
      cellContent.style.visibility = 'hidden';
 
-     // poplulate cells w icons, rows w cells, flexContain w rows
+     // poplulate cells w icons, rows w cells, flexTable w rows
      let cellCount = 1;
      for (let i of iconIndexDupeArr) {
          cellContent.classList = `${icons[i]} ${cellCount}`;
          cell.appendChild(cellContent);
          row.appendChild(cell.cloneNode(true))
          if (cellCount % 4 == 0) {
-             flexContain.appendChild(row.cloneNode(true));
+             flexTable.appendChild(row.cloneNode(true));
              row.innerHTML = null;
          }
          cellCount++;
      };
-     // const stats = document.querySelector('#stats');
-     // stats.prepend(flexContain);
-     mainContain.appendChild(flexContain);
+     mainContain.appendChild(flexTable);
  };
-
+ // on page load
  document.addEventListener('DOMContentLoaded', () => {
      makeGrid()
  });
 
- // stats
- let totalClicks = 0;
- const clickCountCell = document.querySelector('#click-count-cell');
- clickCountCell.innerText = `${totalClicks} clicks`;
+ const newGameBtn = document.querySelector('#new-game-btn');
+ newGameBtn.addEventListener('click', () => {
+     clearInterval(startClock);
+     flexTable.innerHTML = null;
+     makeGrid();
+ });
 
- let userMatches = 0;
- const gameClockCell = document.querySelector('#game-clock-cell')
- let userStartTime, startClock;
- // userStartTime = 0;
- const getStartTime = () => userStartTime = Date.now();
+ const gameClock = () => gameClockDisplay.innerText = `time :${Math.floor((Date.now() - startTime) / 1000)}`;
 
- const gameClock = () => gameClockCell.innerText = `seconds :${Math.floor((Date.now() - userStartTime) / 1000)}`;
- gameClock();
+ let firstClick, userMatches, totalClicks, startClock, startTime;
 
- const stopClock = () => clearInterval(startClock);
-
- // const newGameBtn = document.querySelector('#new-game-btn');
- // newGameBtn.addEventListener('click', () => {
- //     flexContain.innerHTML = null;
- //     totalClicks = 0;
- //     makeGrid()
- // });
-
- // track user clicks. click order: if no match lastClick becomes firstClick
- let lastClick, firstClick;
- flexContain.addEventListener('click', (e) => {
-     if (e.target.className = 'cell') {
-         lastClick = e.target.firstChild;
+ // start/stop stats, track user clicks, click order: if no match lastClick becomes firstClick
+ flexTable.addEventListener('click', (e) => {
+     if (e.target.className != 'cell') {
+         return;
+     } else {
+         // track user clicks
+         let lastClick = e.target.firstChild;
          lastClick.style.visibility = 'visible';
-
-         // start user stats
+         // start game stats
          if (totalClicks == 0) {
-             getStartTime();
+             startTime = Date.now();
              startClock = setInterval(gameClock, 1000);
          }
          totalClicks++;
-         clickCountCell.innerText = `${totalClicks} clicks`;
+         clickCountDisplay.innerText = `${totalClicks} clicks`;
 
          if (firstClick != null && firstClick.classList[1] == lastClick.classList[1]) {
              // consecutive clicks on same cell do not return match
@@ -91,7 +85,8 @@
                  firstClick.style.visibility = 'visible';
                  userMatches++;
                  if (userMatches == icons.length) {
-                     return stopClock();
+                     // stop clock cuz game over
+                     clearInterval(startClock);
                  }
                  return firstClick = null;
              }
