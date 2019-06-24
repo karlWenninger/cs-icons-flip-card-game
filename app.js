38cef9032f40c3ee5e826fd2891ea1a64961e41a
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-24 16:58:05 -0400

    Complied JS, CSS and HTML to linter suggestions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
- const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"]
+ const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"];
 
  const mainContain = document.querySelector('#main-contain');
 
  const clickCountDisplay = document.querySelector('#click-count-display');
 
- const gameClockDisplay = document.querySelector('#game-clock-display')
+ const gameClockDisplay = document.querySelector('#game-clock-display');
 
  const flexTable = document.createElement('div');
  flexTable.className = 'flex-table';
@@ -23,9 +23,9 @@
      let iconIndexArr = [];
      for (let i in icons) {
          iconIndexArr.push(i);
-     };
+     }
      // dupe entire array (each icon loads in grid twice)
-     iconIndexDupeArr = iconIndexArr.concat(iconIndexArr);
+     let iconIndexDupeArr = iconIndexArr.concat(iconIndexArr);
      // random sort indexes
      iconIndexDupeArr.sort((a, b) => 0.5 - Math.random());
      // create HTML elements
@@ -41,30 +41,30 @@
      for (let i of iconIndexDupeArr) {
          cellContent.classList = `${icons[i]} ${cellCount}`;
          cell.appendChild(cellContent);
-         row.appendChild(cell.cloneNode(true))
+         row.appendChild(cell.cloneNode(true));
          if (cellCount % 4 == 0) {
              flexTable.appendChild(row.cloneNode(true));
              row.innerHTML = null;
          }
          cellCount++;
-     };
+     }
      mainContain.appendChild(flexTable);
- };
+ }
 
 
  function gameClock() {
      const timeElapsed = Math.floor((Date.now() - startTime) / 1000);
-     if (timeElapsed < 45) {
-         gameClockDisplay.innerText = `time 0:0${timeElapsed}`
+     if (timeElapsed < 10) {
+         gameClockDisplay.innerText = `time 0:0${timeElapsed}`;
      } else {
-         gameClockDisplay.innerText = `time 0:${timeElapsed}`
+         gameClockDisplay.innerText = `time 0:${timeElapsed}`;
      }
      if (timeElapsed == 45) {
          flexTable.style.opacity = '0.4';
          clearInterval(startClock);
          timesUpRule.className = 'underline-flash';
          return timesUp = true;
-     };
+     }
  }
 
 
@@ -100,13 +100,13 @@
          } else { firstClick = lastClick; }
          setTimeout(() => {
              return lastClick.style.visibility = 'hidden';
-         }, 500)
+         }, 500);
      }
  });
 
  // on page load
  document.addEventListener('DOMContentLoaded', () => {
-     makeGrid()
+     makeGrid();
  });
 
  const newGameBtn = document.querySelector('#new-game-btn');
