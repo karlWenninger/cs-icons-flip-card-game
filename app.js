e04c748ea601336827756649aac9ed0ff5d2cf67
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-24 20:21:51 -0400

    Minor CSS spacing / alignment touch ups

--- app.js
+++ app.js
@@ -1,120 +1,116 @@
- const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"];
+const mainContain = document.querySelector('#main-contain');
 
- const mainContain = document.querySelector('#main-contain');
+const clickCountDisplay = document.querySelector('#click-count-display');
 
- const clickCountDisplay = document.querySelector('#click-count-display');
+const gameClockDisplay = document.querySelector('#game-clock-display');
 
- const gameClockDisplay = document.querySelector('#game-clock-display');
+const timesUpLi = document.querySelector('#times-up-li');
 
- const flexTable = document.createElement('div');
- flexTable.className = 'flex-table';
+const flexTable = document.querySelector('#flex-table');
 
- const timesUpRule = document.querySelector('#times-up-rule');
+let firstClick, userMatches, totalClicks, startClock, startTime, timesUp;
 
- function makeGrid() {
-     // set game start defaults
-     totalClicks = 0;
-     clickCountDisplay.innerText = `${totalClicks} clicks`;
-     gameClockDisplay.innerText = 'time 0:00';
-     userMatches = 0;
-     timesUp = false;
+const icons = ["fas fa-tree", "fas fa-bug", "fas fa-list", "fas fa-link", "fas fa-sync-alt", "fas fa-key", "fas fa-car-crash", "fas fa-sort-numeric-down"];
 
-     // make icon index array
-     let iconIndexArr = [];
-     for (let i in icons) {
-         iconIndexArr.push(i);
-     }
-     // dupe entire array (each icon loads in grid twice)
-     let iconIndexDupeArr = iconIndexArr.concat(iconIndexArr);
-     // random sort indexes
-     iconIndexDupeArr.sort((a, b) => 0.5 - Math.random());
-     // create HTML elements
-     const row = document.createElement('div');
-     row.className = 'row';
-     const cell = document.createElement('div');
-     cell.className = 'cell';
-     const cellContent = document.createElement('i');
-     cellContent.style.visibility = 'hidden';
+function makeGrid() {
+    // set game start defaults
+    totalClicks = 0;
+    clickCountDisplay.innerText = `${totalClicks} clicks`;
+    gameClockDisplay.innerText = 'time 0:00';
+    userMatches = 0;
+    timesUp = false;
+    timesUpLi.className = null;
+    clearInterval(startClock);
+    flexTable.innerHTML = null;
+    flexTable.style.opacity = '1';
 
-     // poplulate cells w icons, rows w cells, flexTable w rows
-     let cellCount = 1;
-     for (let i of iconIndexDupeArr) {
-         cellContent.classList = `${icons[i]} ${cellCount}`;
-         cell.appendChild(cellContent);
-         row.appendChild(cell.cloneNode(true));
-         if (cellCount % 4 == 0) {
-             flexTable.appendChild(row.cloneNode(true));
-             row.innerHTML = null;
-         }
-         cellCount++;
-     }
-     mainContain.appendChild(flexTable);
- }
+    // make icon index array
+    let iconIndexArr = [];
+    for (let i in icons) {
+        iconIndexArr.push(i);
+    }
+    // dupe entire array (each icon loads in flexTable twice)
+    let iconIndexDupeArr = iconIndexArr.concat(iconIndexArr);
+    // random sort indexes
+    iconIndexDupeArr.sort((a, b) => 0.5 - Math.random());
+    // create HTML elements
+    const row = document.createElement('div');
+    row.className = 'row';
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    const cellContent = document.createElement('i');
+    cellContent.style.visibility = 'hidden';
 
+    // poplulate cells w icons, rows w cells, flexTable w rows
+    let cellCount = 1;
+    for (let i of iconIndexDupeArr) {
+        cellContent.classList = `${icons[i]} ${cellCount}`;
+        cell.appendChild(cellContent);
+        row.appendChild(cell.cloneNode(true));
+        if (cellCount % 4 == 0) {
+            flexTable.appendChild(row.cloneNode(true));
+            row.innerHTML = null;
+        }
+        cellCount++;
+    }
+    mainContain.appendChild(flexTable);
+}
 
- function gameClock() {
-     const timeElapsed = Math.floor((Date.now() - startTime) / 1000);
-     if (timeElapsed < 10) {
-         gameClockDisplay.innerText = `time 0:0${timeElapsed}`;
-     } else {
-         gameClockDisplay.innerText = `time 0:${timeElapsed}`;
-     }
-     if (timeElapsed == 45) {
-         flexTable.style.opacity = '0.4';
-         clearInterval(startClock);
-         timesUpRule.className = 'underline-flash';
-         return timesUp = true;
-     }
- }
+function gameClock() {
+    const timeElapsed = Math.floor((Date.now() - startTime) / 1000);
+    if (timeElapsed < 10) {
+        gameClockDisplay.innerText = `time 0:0${timeElapsed}`;
+    } else {
+        gameClockDisplay.innerText = `time 0:${timeElapsed}`;
+    }
+    if (timeElapsed == 45) {
+        timesUpLi.className = 'underline-flash';
+        flexTable.style.opacity = '0.5';
+        clearInterval(startClock);
+        return timesUp = true;
+    }
+}
 
+flexTable.addEventListener('click', (e) => {
+    // exit func
+    if (e.target.className != 'cell' || timesUp == true) {
+        return;
+    } else {
+        // start gameClock
+        if (totalClicks == 0) {
+            startTime = Date.now();
+            startClock = setInterval(gameClock, 1000);
+        }
+        totalClicks++;
+        clickCountDisplay.innerText = `${totalClicks} clicks`;
 
- let firstClick, userMatches, totalClicks, startClock, startTime, timesUp;
+        // track user clicks: if no match lastClick becomes firstClick
+        const lastClick = e.target.firstChild;
+        lastClick.style.visibility = 'visible';
 
- // start/stop stats, track user clicks, click order: if no match lastClick becomes firstClick
- flexTable.addEventListener('click', (e) => {
-     if (e.target.className != 'cell' || timesUp == true) {
-         return;
-     } else {
-         // track user clicks
-         let lastClick = e.target.firstChild;
-         lastClick.style.visibility = 'visible';
-         // start game stats
-         if (totalClicks == 0) {
-             startTime = Date.now();
-             startClock = setInterval(gameClock, 1000);
-         }
-         totalClicks++;
-         clickCountDisplay.innerText = `${totalClicks} clicks`;
+        if (firstClick != null && firstClick.classList[1] == lastClick.classList[1]) {
+            // consecutive clicks on same cell do not return match
+            if (firstClick.classList[2] != lastClick.classList[2]) {
+                firstClick.style.visibility = 'visible';
+                userMatches++;
+                if (userMatches == icons.length) {
+                    // stop clock cuz game over
+                    clearInterval(startClock);
+                }
+                return firstClick = null;
+            }
+        } else { firstClick = lastClick; }
+        setTimeout(() => {
+            return lastClick.style.visibility = 'hidden';
+        }, 500);
+    }
+});
 
-         if (firstClick != null && firstClick.classList[1] == lastClick.classList[1]) {
-             // consecutive clicks on same cell do not return match
-             if (firstClick.classList[2] != lastClick.classList[2]) {
-                 firstClick.style.visibility = 'visible';
-                 userMatches++;
-                 if (userMatches == icons.length) {
-                     // stop clock cuz game over
-                     clearInterval(startClock);
-                 }
-                 return firstClick = null;
-             }
-         } else { firstClick = lastClick; }
-         setTimeout(() => {
-             return lastClick.style.visibility = 'hidden';
-         }, 500);
-     }
- });
+const newGameBtn = document.querySelector('#new-game-btn');
+newGameBtn.addEventListener('click', () => {
+    makeGrid();
+});
 
- // on page load
- document.addEventListener('DOMContentLoaded', () => {
-     makeGrid();
- });
-
- const newGameBtn = document.querySelector('#new-game-btn');
- newGameBtn.addEventListener('click', () => {
-     timesUpRule.className = null;
-     timesUp = false;
-     clearInterval(startClock);
-     flexTable.innerHTML = null;
-     flexTable.style.opacity = '1';
-     makeGrid();
- });
+document.addEventListener('DOMContentLoaded', () => {
+    makeGrid();
+});
